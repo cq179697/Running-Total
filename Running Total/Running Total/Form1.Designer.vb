0c530750f919a25dfe85dbfe8e9b26c18af580bc
cq179697 <bondook714@yahoo.com>
2016-09-29 11:16:49 -0500

    Add files via upload

---
+++ Running Total/Running Total/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnEnterSales = New System.Windows.Forms.Button()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnEnterSales
+        '
+        Me.btnEnterSales.Location = New System.Drawing.Point(12, 46)
+        Me.btnEnterSales.Name = "btnEnterSales"
+        Me.btnEnterSales.Size = New System.Drawing.Size(75, 23)
+        Me.btnEnterSales.TabIndex = 0
+        Me.btnEnterSales.Text = "Enter Sales"
+        Me.btnEnterSales.UseVisualStyleBackColor = True
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BackColor = System.Drawing.SystemColors.Control
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Location = New System.Drawing.Point(71, 9)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(197, 34)
+        Me.lblTotal.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(2, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Total Sales:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(193, 46)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.ClientSize = New System.Drawing.Size(280, 76)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.btnEnterSales)
+        Me.Name = "Form1"
+        Me.Text = "Total Sales"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnEnterSales As System.Windows.Forms.Button
+    Friend WithEvents lblTotal As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
